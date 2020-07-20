bc5063f2e79178671e179983bacc941fc53eccf3
Artemie Amari <yaroamari@gmail.com>
2020-07-20 17:01:26 -0400

    auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Container from './Container'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>OctaGo</h1>
+      <Container />
     </div>
   );
 }
---
+++ client/src/Container.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import {Router} from 'react-router'
+
+export default class Container extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
