6bc26a79bb45d59c14d73f3cd4769d287cfd7f5c
Artemie Amari <yaroamari@gmail.com>
2020-07-21 14:51:54 -0400

    Login

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react'
-import Bar from '../images/download.png'
+import {Link} from 'react-router-dom'
 
 export default class Nav extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      toggle: false
+    }
+  }
   render() {
     return (
-      <div>
-        <img src={Bar} />
+      <div class="nav">
+        < a href="javascript:void(0);" class="icon"
+          onClick={() => { this.setState({ toggle: !this.state.toggle})}}>
+          <i class="fa fa-bars"></i>
+          <div class="nav-items">
+          {this.state.toggle
+            ?
+            <div class="links">
+              <Link to="/" class="nav-link">Home</Link>
+              <Link to="/events" class="nav-link">Events</Link>
+              {this.props.currentUser
+                ?
+                <Link to="/logout" class="nav-link">Logout</Link>
+                :
+                <Link to="/login" class="nav-link">Login</Link>
+          }
+            </div>
+            :
+            null
+            }
+            </div>
+          </a>
       </div>
     )
   }
