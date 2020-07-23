c95dc52c2b8559c11bcd21d75c73c4e406861c9f
llukkah <llukkah.rey@gmail.com>
2020-07-23 16:47:24 -0400

    added intro file

---
+++ client/src/components/Intro.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import 'react-slideshow-image/dist/styles.css'
+import Intro1 from '/images/Intro1.png'
+import Intro2 from '/images/Intro2.png'
+import Intro3 from '/images/Intro3.png'
+import Intro4 from '/images/Intro4.png'
+import Intro5 from '/images/Intro5.png'
+import Intro6 from '/images/Intro6.png'
+
+
+export default function Intro () {
+    return (
+
+
+
+
+
+
+
+
+
+
+
+
+
+    );
+ }
